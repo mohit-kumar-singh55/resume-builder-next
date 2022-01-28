5ded7c1c3398d8c9493adbd824d8a56d6a609fdf
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-01-28 20:00:23 +0530

    something is not working

--- components/Body/Body.jsx
+++ components/Body/Body.jsx
@@ -16,6 +16,45 @@ function Header() {
         other: "Other",
     };
 
+    const [resumeInformation, setResumeInformation] = useState({
+        [sections.basicInfo]: {
+            id: sections.basicInfo,
+            sectionTitle: sections.basicInfo,
+            detail: {},
+        },
+        [sections.workExp]: {
+            id: sections.workExp,
+            sectionTitle: sections.workExp,
+            details: [],
+        },
+        [sections.project]: {
+            id: sections.project,
+            sectionTitle: sections.project,
+            details: [],
+        },
+        [sections.education]: {
+            id: sections.education,
+            sectionTitle: sections.education,
+            details: [],
+        },
+        [sections.achievement]: {
+            id: sections.achievement,
+            sectionTitle: sections.achievement,
+            points: [],
+        },
+        [sections.summary]: {
+            id: sections.summary,
+            sectionTitle: sections.summary,
+            detail: "",
+        },
+        [sections.other]: {
+            id: sections.other,
+            sectionTitle: sections.other,
+            detail: "",
+        },
+    });
+
+
     return (
         <div className={styles.container}>
             <h2 className={styles.heading}>Resume Builder</h2>
