78261d71dd838111e317660d336f20d26574eeff
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-01-29 22:14:56 +0530

    sitemap

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
         <div className={styles.container}>
             <div className={styles.left}>
                 <h1 className={styles.heading}>Resume.MKS</h1>
-                <p className={styles.heading}>Make your resume easily.<span>It&apos;s free</span></p>
+                <p className={styles.heading}>Make your resume easily.<span> It&apos;s free</span></p>
             </div>
             <div className={styles.right}>
                 <Image src={resumeSVG} alt="Resume" width="450px" />
